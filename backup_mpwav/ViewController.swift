127aa84bd96445a63a0e0b01d5202067c9a8bacf
donggeon9999 <119ldk@naver.com>
2023-08-14 16:16:00 +0900

    Initial Commit

---
+++ backup_mpwav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  backup_mpwav
+//
+//  Created by 이동건 on 2023/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ backup_mpwavTests/backup_mpwavTests.swift
@@ -0,0 +1,36 @@
+//
+//  backup_mpwavTests.swift
+//  backup_mpwavTests
+//
+//  Created by 이동건 on 2023/08/14.
+//
+
+import XCTest
+@testable import backup_mpwav
+
+final class backup_mpwavTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
